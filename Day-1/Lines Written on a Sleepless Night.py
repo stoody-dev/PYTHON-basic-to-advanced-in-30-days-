87cac8f6496a04e539f79adb84c4e3c4f985ee9c
Akshay Chaudhary <forakshay.dev@gmail.com>
2024-03-13 00:00:30 +0530

    1.0

---
+++ Day-1/Lines Written on a Sleepless Night.py
@@ -0,0 +1,44 @@
+from re import I
+
+
+print(
+'''
+ I’m unfamiliar with statistics,
+I wouldn’t know what to do
+With a book on Mathematics
+Or a girl of ninety-two.
+
+I really can’t tell the difference
+Between a man from Kalamazoo
+And the kind of endangered species
+That you only find in a zoo.
+
+I’m hopeless at Nuclear Physics-
+Don’t ask me to make you a bomb-
+But if you would like me to bake you a cake,
+I’ll do it with great aplomb.
+
+I’m not very good at book-keeping,
+My accounting, they say, is too lax.
+I can’t trace my Income, or, credit my debit,
+So how can I pay Income Tax?
+
+I’m really not bad at prognosis,
+Consult me- I won’t take a fee
+I’ll soon let you know if your calcium is low
+Or if it’s just Housemaid’s Knee.
+
+I’m not very good with a Nurse
+And I feel more at ease writing verse-
+I am inclined to convulse when she feels for my pulse,
+And if I feel hers, she gets terse!
+
+I’m hopeless at counting those sheep-
+I’d rather be off with Bo-peep!
+If she’d leave them alone
+And take me straight home
+I wouldn’t mind losing more sleep.
+
+-By Ruskin Bond
+'''
+    )
