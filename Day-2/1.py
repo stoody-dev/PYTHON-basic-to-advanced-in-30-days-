1988268479a7a865f2fab1a888e1d8b29b0b9478
Akshay Chaudhary <forakshay.dev@gmail.com>
2024-03-13 13:47:23 +0530

    2.0

---
+++ Day-2/1.py
@@ -0,0 +1,19 @@
+print("this is a new challange")
+print(3+4)
+
+
+#this is a comment 
+#--------------------------------------------------------------------------------
+a=12
+print(a)
+b=2
+print(a+b)
+print(type(a))
+c = "stoody"
+print(c)
+print(type(c))
+d= True
+print(d, type(d))
+list1 = [4,4.3,[-3,2],['apple'], [] ]
+print(list1)
+
