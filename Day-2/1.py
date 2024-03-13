6e3e50d54c7936e0bf4f5601bb21978b50e39e2c
Akshay Chaudhary <forakshay.dev@gmail.com>
2024-03-13 14:30:39 +0530

    2.1.1

--- Day-2/1.py
+++ Day-2/1.py
@@ -11,15 +11,3 @@ d= True
 print(d, type(d))
 list1 = [4,4.3,[-3,2],['apple'], [] ]
 print(list1)
-
-#taking inputs from users 
-a=input()
-print(a)
-
-b=input("Enter your name: ")
-print("My Name is",b)
-
-#arithmatic integers between two integers 
-x=input("enter first number:")
-y=input("enter second solution:")
-print(int(x)+int(y))#typecasting required 
---
+++ Day-2/input.py
@@ -0,0 +1,11 @@
+#taking inputs from users 
+a=input()
+print(a)
+
+b=input("Enter your name: ")
+print("My Name is",b)
+
+#arithmatic integers between two integers 
+x=input("enter first number:")
+y=input("enter second solution:")
+print(int(x)+int(y))#typecasting required 
