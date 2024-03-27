a6f39df63a3efdf6a390954be6ae671365b6bb02
Akshay Chaudhary <forakshay.dev@gmail.com>
2024-03-28 01:05:33 +0530

    day - 4 updated

---
+++ Day-4/1.py
@@ -0,0 +1,20 @@
+'''
+Q1. Read radious of a circle and side of a square from user using single input() method. 
+Then calculate and display area of the circle and the square.
+
+Input:
+3.33 3.33
+
+Output:
+Area of circle:34.82
+Area of square:11.09
+
+Hint:
+Use float() to convert string to float data type.
+'''
+a= float(input('enter the side '))
+print(a)
+
+
+print ('area of circle is ' , 3.14*a*a , "\n area of the square is ", a*a)
+
---
+++ Day-4/2.py
@@ -0,0 +1,14 @@
+'''
+Q2. Write a program that reads names of two python files and strips the extension. The filenames are then concatenated to form a single string. The program then 
+prints 'True' if the newly generated string is palindrome, or prints 'False' otherwise.
+
+Input: malay.py aLam.py
+
+malay alam
+
+malayalam
+
+malayalam
+
+Output: True
+'''
