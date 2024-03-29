90803ccad1b4d857a27615123408c60ccfa88c89
Akshay Chaudhary <forakshay.dev@gmail.com>
2024-03-29 20:03:20 +0530

    5.2

--- Day-5/string operations.py
+++ Day-5/string operations.py
@@ -1,3 +1,26 @@
 #operations on strings 
 
 a = "stoody"
+print (len(a))
+
+print(a.upper())
+print(a.lower())
+
+#strings are immmutable . Isme upper aur lower se strings me koi change nahi ayega . basically a change nahi hota . new string ban jaati hai string methods se 
+
+
+str2 = "helloooo!!!"
+print (str2.strip("!"))#remove only trailing characters . 
+
+print(a.replace("stoody" ,"sawoo"))
+print(a.split("o"))
+
+print(len(a.center(12)))
+
+print(a.endswith("y"))#checks if the string ends with a particular character 
+
+b = "welcome to the jungle"
+print(b.endswith("to",4,10))
+print(a.find("o"))# searches for the 1st occurence and returns its index .
+
+print(a.index("o"))
