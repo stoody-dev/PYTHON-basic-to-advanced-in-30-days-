e350b984713708fb1dfadab573a04d448ce7148e
Akshay Chaudhary <forakshay.dev@gmail.com>
2024-04-03 00:57:55 +0530

    Create functions.py

---
+++ Day-7/functions.py
@@ -0,0 +1,20 @@
+def isgreater (a,b):
+    if (a>b):
+        print ("a is greater")
+
+    elif(b>a):
+        print ("b is greater")    
+
+    else:
+        print("dono equal hai buddhu")    
+
+
+
+
+
+a = int(input("enter first number"))
+
+b = int(input("enter second number "))
+
+
+print("the greater number among a and b is", isgreater(a,b))
