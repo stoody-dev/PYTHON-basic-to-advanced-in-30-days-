3e7198592fae0a92840d9973d65bd567b252e91f
Akshay Chaudhary <forakshay.dev@gmail.com>
2024-04-03 00:51:09 +0530

    7.0

---
+++ Day-5/1.py
@@ -0,0 +1,18 @@
+name = 'akshay '
+for i in name :
+    print (i)
+
+
+
+for i in range(5):
+    print (i+1)
+           
+           
+           
+ 
+ 
+ 
+count = 5 
+while (count>0): 
+    print (count)
+    count = count -1
---
+++ Day-7/patterns/1.py
@@ -0,0 +1,17 @@
+# for i in range (8):
+#     for j in range(i):
+#         print (i,end = '')
+#     print("")    
+
+
+for i in range(5):
+    for j in range(i):
+        print('*' ,end='')
+    print("")  
+    
+
+
+
+
+
+    
